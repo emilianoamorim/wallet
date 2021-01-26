e525919792f1ddd7bddf1fa247cb6b8e7b47c67e
Emiliano Correia <emilianocorreiadasilva@gmail.com>
2021-01-26 16:32:05 -0300

    Add routes from auth and customers

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,13 +1,54 @@
 
 const routes = [
+  // Auth
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    alias: '/login',
+    path: '/signin',
+    component: () => import('layouts/AuthLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') }
+      { path: '', component: () => import('pages/Auth/Signin.vue') }
     ]
   },
-
+  {
+    alias: '/cadastro',
+    path: '/signup',
+    component: () => import('layouts/AuthLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/Auth/Signup.vue') }
+    ]
+  },
+  {
+    alias: '/recuperar-senha',
+    path: '/recover',
+    component: () => import('layouts/AuthLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/Auth/Recover.vue') }
+    ]
+  },
+  // Dashboard
+  {
+    path: '/',
+    component: () => import('layouts/DashBoardLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/Dashboard/Index.vue') }
+    ]
+  },
+  {
+    alias: '/clientes',
+    path: '/customers',
+    component: () => import('layouts/DashBoardLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/Dashboard/Customers.list.vue') }
+    ]
+  },
+  {
+    alias: '/clientes/editar/:id(\\d+)',
+    path: '/customers/edit/:id(\\d+)',
+    component: () => import('layouts/DashBoardLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/Dashboard/Customers.edit.vue') }
+    ]
+  },
   // Always leave this as last one,
   // but you can also remove it
   {
