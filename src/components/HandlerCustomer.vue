46313aab1dc8a245279b97261df482c35ab8759e
Emiliano Correia <emilianocorreiadasilva@gmail.com>
2021-01-27 18:54:14 -0300

    Quasar components

---
+++ src/components/HandlerCustomer.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <q-dialog v-model="modalHandlerCustomer">
+      <q-card>
+        <q-card-section>
+          <div class="text-h6">Alert</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet porro. Rerum blanditiis perferendis totam, ea at omnis vel numquam exercitationem aut, natus minima, porro labore.
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn @click="handlerCustomerMutation(false)" flat label="OK" color="primary" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+  </div>
+</template>
+
+<script>
+import { mapState, mapMutations } from 'vuex'
+export default {
+  data: () => ({
+    handler: false
+  }),
+  computed: {
+    ...mapState('example', {
+      modalHandlerCustomer: state => state.modalHandlerCustomer
+    })
+  },
+  methods: {
+    ...mapMutations('example', ['handlerCustomerMutation'])
+  }
+}
+</script>
--- src/layouts/DashBoardLayout.vue
+++ src/layouts/DashBoardLayout.vue
@@ -4,7 +4,24 @@
       <q-header elevated class="bg-black">
         <q-toolbar>
           <q-btn flat @click="drawer = !drawer" round dense icon="menu" />
-          <q-toolbar-title>Header</q-toolbar-title>
+          <q-toolbar-title>Dashboard</q-toolbar-title>
+          <q-btn flat round dense icon="person">
+            <q-menu>
+              <q-list style="min-width: 100px">
+                <q-item clickable v-close-popup>
+                  <q-item-section>Minha conta</q-item-section>
+                </q-item>
+                <q-item clickable v-close-popup>
+                  <q-item-section>Alterar senha</q-item-section>
+                </q-item>
+                <q-separator />
+                <q-item clickable v-close-popup>
+                  <q-item-section>Configurações</q-item-section>
+                </q-item>
+              </q-list>
+            </q-menu>
+          </q-btn>
+          <q-btn flat round dense icon="power_settings_new" />
         </q-toolbar>
       </q-header>
 
@@ -17,45 +34,25 @@
         content-class="bg-grey-3">
         <q-scroll-area class="fit">
           <q-list padding class="menu-list">
-            <q-item clickable v-ripple>
-              <q-item-section avatar>
-                <q-icon name="inbox" />
-              </q-item-section>
 
+            <q-item clickable v-ripple to="/?_ref=nav">
+              <q-item-section avatar>
+                <q-icon name="dashboard" />
+              </q-item-section>
               <q-item-section>
-                Inbox
+                Inicio
               </q-item-section>
             </q-item>
 
-            <q-item active clickable v-ripple>
+            <q-item clickable v-ripple to="/customers">
               <q-item-section avatar>
-                <q-icon name="star" />
+                <q-icon name="supervisor_account" />
               </q-item-section>
-
               <q-item-section>
-                Star
+                Clientes
               </q-item-section>
             </q-item>
 
-            <q-item clickable v-ripple>
-              <q-item-section avatar>
-                <q-icon name="send" />
-              </q-item-section>
-
-              <q-item-section>
-                Send
-              </q-item-section>
-            </q-item>
-
-            <q-item clickable v-ripple>
-              <q-item-section avatar>
-                <q-icon name="drafts" />
-              </q-item-section>
-
-              <q-item-section>
-                Drafts
-              </q-item-section>
-            </q-item>
           </q-list>
         </q-scroll-area>
       </q-drawer>
